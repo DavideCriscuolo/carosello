852fb3c3fad5a39950d310bd4c69a45d249f2895
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-12 12:44:53 +0200

    Correggi errori di battitura e migliora la struttura del carosello

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,110 +1,131 @@
-// Come creare  un carosello dinamico
-//  Creaiamo un array di oggetti con proprità: img, title, desc
-
+// crate an array of objects with the following properties: image, title, description
 const images = [
   {
     image: "1.avif",
     title: "Expedition 33",
-    description: "Lorem game 123",
+    desciption: "Lorem game 123",
   },
   {
     image: "2.jpg",
-    title: "Lies  of P",
-    description: "Lorem game P",
+    title: "Lies of P",
+    desciption: "Lorem game P",
   },
   {
     image: "3.webp",
     title: "Sekiro",
-    description: "Lorem game Sekiro",
+    desciption: "Lorem game Sekiro",
   },
   {
     image: "4.avif",
-    title: "Jedi Survivor",
-    description: "Lorem game jedi",
+    title: "Jedi survivor",
+    desciption: "Lorem game Jedi",
   },
   {
     image: "5.jpeg",
-    title: "Street Fighter",
-    description: "Lorem game street",
+    title: "Street fighter",
+    desciption: "Lorem game street f",
   },
 ];
 
-//Prendiamo gli elementi della dom : freccia prev, freccia next, img, thumbnails
-
-const backwardEL = document.querySelector(".backward");
+// select the required dom elements: prev, next, image, thumbnails
+const backwardEl = document.querySelector(".backward");
 const forwardEl = document.querySelector(".forward");
-const imageEL = document.querySelector(".image");
-const thumbnailsEL = document.querySelector(".thumbnails");
+const imageEl = document.querySelector(".image");
+const thumbnailsEl = document.querySelector(".thumbnails");
+const carouselEl = document.querySelector(".carosello");
 
 let activeImage = 0;
-// renderizzare la prima immagine nella dom
 
+// render the first image to the dom
+renderPreviewImage(activeImage, images, imageEl);
+
+// add an event listener to the forward element
+forwardEl.addEventListener("click", next);
+
+// add an event listener to the backward element
+
+backwardEl.addEventListener("click", prev);
+
+// 📌 thumbnails
+
+// loop over the array of image
+for (let i = 0; i < images.length; i++) {
+  // get the current image object
+  const thumbnail = images[i];
+  // destructure the image object to get title and image values
+  const { image, title } = thumbnail;
+  console.log(image, title);
+  // generate the thumbnails markup (DOM API)
+
+  // create a div element
+  const divEl = document.createElement("div");
+  // add a col class
+  divEl.classList.add("col");
+
+  // create the image element
+  const imgEl = document.createElement("img");
+  // add the img-fluid
+  imgEl.classList.add("img-fluid");
+  imgEl.style.objectFit = "cover";
+  imgEl.style.aspectRatio = "16/9";
+  // add the src attribute with the image path
+  imgEl.src = `./assets/img/${image}`;
+  // add the alt attribute
+  imgEl.alt = title;
+  // data attribute
+  imgEl.setAttribute("data-thumb-id", i);
+
+  // append the image to the col using appendChild
+  divEl.appendChild(imgEl);
+  console.log(divEl);
+
+  // attach an event listener to the dom element
+  imgEl.addEventListener("click", () => {
+    console.log("You clicked a thumb");
+    const thumbId = Number(imgEl.getAttribute("data-thumb-id"));
+    console.log(thumbId);
+    activeImage = thumbId;
+    renderPreviewImage(activeImage, images, imageEl);
+  });
+
+  // Insert the generated markup to the thumbnails dom element
+  thumbnailsEl.appendChild(divEl);
+}
+
+let intervalId = setInterval(next, 1500);
+
+carouselEl.addEventListener("mouseenter", () => {
+  console.log("your entered into the carousel");
+  clearInterval(intervalId);
+});
+
+carouselEl.addEventListener("mouseleave", () => {
+  console.log("your left the carousel");
+
+  intervalId = setInterval(next, 1500);
+});
+
+/* Functions */
 function renderPreviewImage(index, arr, nodeEl) {
   nodeEl.innerHTML = `<img class="img-fluid" src="./assets/img/${arr[index].image}" alt="${arr[index].title}">`;
 }
-renderPreviewImage(activeImage, images, imageEL);
-// aggiungere un eventListner alla freccie
 
-forwardEl.addEventListener("click", function () {
-  console.log("clicked next");
+function next() {
+  console.log("Clicked next");
   activeImage++;
-
   if (activeImage === images.length) {
     activeImage = 0;
   }
   console.log(activeImage);
-  renderPreviewImage(activeImage, images, imageEL);
-});
+  renderPreviewImage(activeImage, images, imageEl);
+}
 
-backwardEL.addEventListener("click", function () {
-  console.log("clicked prev");
+function prev() {
+  console.log("clicked backward");
   activeImage--;
 
   if (activeImage < 0) {
     activeImage = images.length - 1;
   }
-  console.log(activeImage);
-  renderPreviewImage(activeImage, images, imageEL);
-});
-// generare  il markup thumbnails
-
-//Facciamo un cliclo all interno delle immagini e ci prendiamo l oggetto imagine
-
-for (let i = 0; i < images.length; i++) {
-  const thumbnail = images[i];
-
-  //estraiamo dall oggetto immagine  il titolo e l img e poi  generiamo il markup
-
-  const { image, title } = thumbnail;
-  console.log(image, title);
-
-  //creare un elemento div
-  // -aggiungere una clase ,
-  // -creamo l elmento image con classe img-fluid,
-  // -aggiungere il percorso e alt,
-  // -appendere img all interno di col
-
-  const divEl = document.createElement("div");
-
-  divEl.classList.add("col");
-  const imgEl = document.createElement("img");
-  imgEl.classList.add("img-fluid");
-  imgEl.src = `./assets/img/${image}`;
-  imgEl.alt = title;
-  imgEl.id = i;
-  imgEl.setAttribute("data-thumb-id", i);
-  imgEl.style.objectFit = "cover";
-  imgEl.style.aspectRatio = "16/9";
-
-  divEl.appendChild(imgEl);
-  console.log(divEl);
-
-  imgEl.addEventListener("click", function () {
-    const thumbId = Number(imgEl.getAttribute("data-thumb-id"));
-    activeImage = thumbId;
-    renderPreviewImage(activeImage, images, imageEL);
-    console.log(thumbId);
-  });
-
-  thumbnailsEL.appendChild(divEl);
+  renderPreviewImage(activeImage, images, imageEl);
 }
