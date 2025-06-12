95abebd81072bc053e2cb08ac2d13c15a5828a20
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-12 11:02:05 +0200

    Aggiungi immagini e script per il carosello dinamico

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+// Come creare  un carosello dinamico
+//  Creaiamo un array di oggetti con proprità: img, title, desc
+
+const images = [
+  {
+    image: "1.avif",
+    title: "Expedition 33",
+    description: "Lorem game 123",
+  },
+  {
+    image: "2.jpg",
+    title: "Lies  of P",
+    description: "Lorem game P",
+  },
+  {
+    image: "3.webp",
+    title: "Sekiro",
+    description: "Lorem game Sekiro",
+  },
+  {
+    image: "4.avif",
+    title: "Jedi Survivor",
+    description: "Lorem game jedi",
+  },
+  {
+    image: "5.jpeg",
+    title: "Street Fighter",
+    description: "Lorem game street",
+  },
+];
+
+//Prendiamo gli elementi della dom : freccia prev, freccia next, img, thumbnails
+
+const backwardEL = document.querySelector(".backward");
+const forwardEl = document.querySelector(".forward");
+const imageEL = document.querySelector(".image");
+const thumbnailsEL = document.querySelector(".thumbnails");
+
+let activeImage = 0;
+// renderizzare la prima immagine nella dom
+
+function renderPreviewImage(index) {
+  imageEL.innerHTML = `<img class="img-fluid" src="./assets/img/${images[index].image}" alt="">`;
+}
+renderPreviewImage(activeImage);
+// aggiungere un eventListner alla freccie
+
+forwardEl.addEventListener("click", function () {
+  console.log("clicked next");
+  activeImage++;
+
+  if (activeImage === images.length) {
+    activeImage = 0;
+  }
+  console.log(activeImage);
+  renderPreviewImage(activeImage);
+});
+
+backwardEL.addEventListener("click", function () {
+  console.log("clicked prev");
+  activeImage--;
+
+  if (activeImage < 0) {
+    activeImage = images.length - 1;
+  }
+  console.log(activeImage);
+  renderPreviewImage(activeImage);
+});
+// generare  il markup thumbnails
